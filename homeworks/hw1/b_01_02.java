74ed7694817aa7c58780344048bbc07145171505
Anna Aleksandrova <anyaaleksandrova1000@gmail.com>
2026-01-02 13:36:19 +0200

    rearranges files

--- homeworks/hw1/b_01_02.java
+++
@@ -1,8 +0,0 @@
-public class b_01_02 {
-    public static void main(String[] args) {
-        int N = args.length;
-        for (int i = 0; i < N; i++) {
-            System.out.println(args[N - i - 1]);
-        }
-    }
-}
